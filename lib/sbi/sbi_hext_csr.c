0a1486b8a2b8d1cca47c29e6e399196b922147bc
dramforever <dramforever@live.com>
2022-04-02 00:19:43 +0800

    lib: sbi: hext: Split into files

---
+++ lib/sbi/sbi_hext_csr.c
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <sbi/sbi_hext.h>
+#include <sbi/sbi_console.h>
+
+int sbi_hext_csr_read(int csr_num, struct sbi_trap_regs *regs, unsigned long *csr_val) {
+	sbi_printf("%s: CSR read 0x%03x: Not implemented\n", __func__, csr_num);
+	return SBI_ENOTSUPP;
+}
+
+int sbi_hext_csr_write(int csr_num, struct sbi_trap_regs *regs, unsigned long csr_val) {
+	sbi_printf("%s: CSR write 0x%03x: Not implemented\n", __func__, csr_num);
+	return SBI_ENOTSUPP;
+}
