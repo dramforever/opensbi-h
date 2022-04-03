d4d40b6e14fd84a33fdc8074027c77fdfec07e44
dramforever <dramforever@live.com>
2022-04-03 12:13:13 +0800

    hext: Run clang-format on modified files

--- lib/sbi/sbi_hext_csr.c
+++ lib/sbi/sbi_hext_csr.c
@@ -5,12 +5,17 @@
 #include <sbi/sbi_hext.h>
 #include <sbi/sbi_console.h>
 
-int sbi_hext_csr_read(int csr_num, struct sbi_trap_regs *regs, unsigned long *csr_val) {
+int sbi_hext_csr_read(int csr_num, struct sbi_trap_regs *regs,
+		      unsigned long *csr_val)
+{
 	sbi_printf("%s: CSR read 0x%03x: Not implemented\n", __func__, csr_num);
 	return SBI_ENOTSUPP;
 }
 
-int sbi_hext_csr_write(int csr_num, struct sbi_trap_regs *regs, unsigned long csr_val) {
-	sbi_printf("%s: CSR write 0x%03x: Not implemented\n", __func__, csr_num);
+int sbi_hext_csr_write(int csr_num, struct sbi_trap_regs *regs,
+		       unsigned long csr_val)
+{
+	sbi_printf("%s: CSR write 0x%03x: Not implemented\n", __func__,
+		   csr_num);
 	return SBI_ENOTSUPP;
 }
