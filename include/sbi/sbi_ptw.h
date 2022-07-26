c5167a0cfd3d4539239556699b23e2304760694e
dramforever <dramforever@live.com>
2022-07-26 23:58:33 +0800

    hext: Add WIP sbi_ptw

---
+++ include/sbi/sbi_ptw.h
@@ -0,0 +1,32 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef __SBI_PTW_H__
+#define __SBI_PTW_H__
+
+#include <sbi/sbi_types.h>
+#include <sbi/sbi_trap.h>
+
+typedef unsigned long sbi_pte_t;
+typedef uint64_t sbi_addr_t;
+
+struct sbi_ptw_csr {
+	unsigned long vsatp;
+	unsigned long hgatp;
+};
+
+typedef sbi_pte_t (*sbi_load_pte_func)(sbi_addr_t addr,
+				       const struct sbi_ptw_csr *csr,
+				       struct sbi_trap_info *trap);
+
+struct sbi_ptw_out {
+	sbi_addr_t base;
+	sbi_addr_t len;
+	sbi_pte_t leaf_pte;
+};
+
+int sbi_ptw_translate(sbi_addr_t gva, const struct sbi_ptw_csr *csr,
+		      struct sbi_ptw_out *out, struct sbi_trap_info *trap);
+
+#endif
