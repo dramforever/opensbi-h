4d492aa8c67b9a0b28142085dca2581979f5a5f2
Vivian Wang <dramforever@live.com>
2022-08-05 21:56:41 +0800

    hext: Check access validity for page faults

--- lib/sbi/sbi_page_fault.c
+++ lib/sbi/sbi_page_fault.c
@@ -36,6 +36,20 @@ static inline ulong convert_access_type(ulong cause, ulong orig_cause)
 	}
 }
 
+static inline sbi_pte_t cause_to_access(unsigned long cause)
+{
+	switch (cause) {
+	case CAUSE_LOAD_PAGE_FAULT:
+		return PTE_R;
+	case CAUSE_STORE_PAGE_FAULT:
+		return PTE_W;
+	case CAUSE_FETCH_PAGE_FAULT:
+		return PTE_X;
+	default:
+		return 0;
+	}
+}
+
 int sbi_page_fault_handler(ulong tval, ulong cause, struct sbi_trap_regs *regs)
 {
 	int ret;
@@ -43,16 +57,27 @@ int sbi_page_fault_handler(ulong tval, ulong cause, struct sbi_trap_regs *regs)
 	struct sbi_ptw_csr csr = { .hgatp = hext->hgatp, .vsatp = hext->vsatp };
 	struct sbi_ptw_out out;
 	struct sbi_trap_info trap;
+	sbi_pte_t access = cause_to_access(cause);
+
 	sbi_printf("%s: page fault 0x%lx cause %d at pc=0x%lx\n", __func__,
 		   tval, (int)cause, regs->mepc);
 	ret = sbi_ptw_translate(tval, &csr, &out, &trap);
+
 	if (ret) {
 		trap.cause = convert_access_type(trap.cause, cause);
 		return sbi_trap_redirect(regs, &trap);
 	}
 
+	if ((access & out.prot) != access) {
+		trap.cause = cause;
+		trap.tval  = tval;
+		trap.tval2 = 0;
+		trap.tinst = 0;
+		return sbi_trap_redirect(regs, &trap);
+	}
+
 	sbi_pt_map(tval, &out, &hext->pt_area);
-	asm volatile("sfence.vma");
+	asm volatile("sfence.vma" ::: "memory");
 
 	return SBI_OK;
 }
